90a4d51803973d1c97c5167661f84c39962467e3
MohitSainiS <mohit.saini.snaatak@mygurukulam.co>
2025-07-03 18:29:30 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,100 +1,111 @@
-variable "name" {
-  description = "Name of the VM"
-  type        = string
-  default     = "default-vm"
+variable "vm_map" {
+  description = "Map of VM configurations keyed by VM name"
+
+  type = map(object({
+    name         = string
+    target_node  = string
+    vm_id        = number
+    cpu_core     = number
+    memory_size  = number
+    ami          = string
+    scsi_hw      = string
+    disk_size    = string
+    storage      = string
+    tags         = string
+  }))
+
+  default = {
+    default-vm = {
+      name         = "default-vm"
+      target_node  = "op-srv-01"
+      vm_id        = 188
+            cpu_core     = 2
+      memory_size  = 2048
+      ami          = "Ubuntu-24"
+      scsi_hw      = "virtio-scsi-single"
+      disk_size    = "30G"
+      storage      = "local"
+      tags         = "dummy;prod"
+    }
+  }
+
   validation {
-    condition     = trim(var.name, "") != ""
+    condition = alltrue([
+      for vm in values(var.vm_map) :
+        trim(vm.name, "") != ""
+    ])
     error_message = "VM name must not be empty or just whitespace."
   }
-}
 
-variable "target_node" {
-  description = "Target Proxmox node"
-  type        = string
-  default     = "op-srv-01"
   validation {
-    condition     = length(var.target_node) > 0
+    condition = alltrue([
+      for vm in values(var.vm_map) :
+        length(vm.target_node) > 0
+    ])
     error_message = "target_node must not be empty."
   }
-}
 
-variable "vm_id" {
-  description = "VM ID"
-  type        = number
-  default     = 101
   validation {
-    condition     = var.vm_id > 100 && var.vm_id < 9999
+    condition = alltrue([
+      for vm in values(var.vm_map) :
+        vm.vm_id > 100 && vm.vm_id < 9999
+    ])
     error_message = "vm_id must be between 101 and 9998."
   }
-}
 
-variable "cpu_core" {
-  description = "CPU cores"
-  type        = number
-  default     = 2
   validation {
-    condition     = var.cpu_core >= 1 && var.cpu_core <= 30
+    condition = alltrue([
+      for vm in values(var.vm_map) :
+        vm.cpu_core >= 1 && vm.cpu_core <= 30
+    ])
     error_message = "cpu_core must be between 1 and 30."
   }
-}
 
-variable "memory_size" {
-  description = "Memory in MB"
-  type        = number
-  default     = 2048
   validation {
-    condition     = var.memory_size >= 1024 && var.memory_size <= 16384
-    error_message = "memory_size must be between 1024 (1 GiB) MiB and 16 GiB (16384 MB)."
+    condition = alltrue([
+      for vm in values(var.vm_map) :
+        vm.memory_size >= 1024 && vm.memory_size <= 16384
+    ])
+    error_message = "memory_size must be between 1024 (1 GiB) and 16384 MB (16 GiB)."
   }
-}
 
-variable "ami" {
-  description = "AMI"
-  type        = string
-  default     = "Ubuntu-24"
   validation {
-    condition     = contains(["Ubuntu-24", "centos-9", "Ubuntu20", "ubuntu-22.04"], var.ami)
-    error_message = "AMI must be one of the following: ubuntu-24, centos-9."
+    condition = alltrue([
+      for vm in values(var.vm_map) :
+        contains(["Ubuntu-24", "centos-9", "Ubuntu20", "ubuntu-22.04"], vm.ami)
+    ])
+    error_message = "AMI must be one of: Ubuntu-24, centos-9, Ubuntu20, ubuntu-22.04."
   }
-}
 
-variable "scsi_hw" {
-  description = "SCSI hardware type"
-  type        = string
-  default     = "virtio-scsi-single"
   validation {
-    condition     = contains(["virtio-scsi-single", "lsi", "scsi-hw"], var.scsi_hw)
+    condition = alltrue([
+      for vm in values(var.vm_map) :
+        contains(["virtio-scsi-single", "lsi", "scsi-hw"], vm.scsi_hw)
+    ])
     error_message = "scsi_hw must be one of: virtio-scsi-single, lsi, scsi-hw."
   }
-}
 
-variable "disk_size" {
-  description = "Disk size"
-  type        = string
-  default     = "30G"
   validation {
-    condition     = can(regex("^\\d+[GM]$", var.disk_size))
+    condition = alltrue([
+      for vm in values(var.vm_map) :
+        can(regex("^\\d+[GM]$", vm.disk_size))
+    ])
     error_message = "disk_size must be in the format like '30G' or '1024M'."
   }
-}
 
-variable "storage" {
-  description = "Storage type"
-  type        = string
-  default     = "local"
   validation {
-    condition     = length(var.storage) > 0
+    condition = alltrue([
+      for vm in values(var.vm_map) :
+        length(vm.storage) > 0
+    ])
     error_message = "storage must not be empty."
   }
-}
 
-variable "tags" {
-  description = "List of Tags 1 to 5"
-  type        = string
-  default     = "dummy;prod"
   validation {
-    condition     = can(regex("^[a-zA-Z0-9;-]+$", var.tags))
+    condition = alltrue([
+      for vm in values(var.vm_map) :
+        can(regex("^[a-zA-Z0-9;-]+$", vm.tags))
+    ])
     error_message = "Tags must be a string like 'dummy;prod' using valid characters."
   }
 }
-
