56ae24c621bc3d085b9bda815d9a818a170ff3a8
MohitSainiS <mohit.saini.snaatak@mygurukulam.co>
2025-07-03 18:28:59 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,13 @@
-output "id" {
-  description = "ID of the proxmox VM"
-  value       = proxmox_vm_qemu.proxmox_vm.id
+output "vm_ids" {
+  description = "VM IDs per instance"
+  value = {
+    for k, vm in proxmox_vm_qemu.proxmox_vm : k => vm.id
+  }
 }
 
-output "vm_state" {
-  description = "State of the proxmox VM"
-  value       = proxmox_vm_qemu.proxmox_vm.vm_state
+output "vm_states" {
+  description = "VM states per instance"
+  value = {
+    for k, vm in proxmox_vm_qemu.proxmox_vm : k => vm.vm_state
+  }
 }
