0e60adc7286e00a2cfdfed859625c04246340059
MohitSainiS <mohit.saini.snaatak@mygurukulam.co>
2025-07-14 16:11:00 +0530

    Update local.tf

--- local.tf
+++ local.tf
@@ -1,5 +1,5 @@
 locals {
-  vm_name = "${var.environment}-${var.location}-${var.role}-${var.identifier}-${var.vertical}-${vm.id}"
+  vm_name = "${var.environment}-${var.location}-${var.role}-${var.identifier}-${var.vertical}"
 
   all_tags = "${var.environment};${var.location};${var.role};${var.identifier};${var.vertical};${var.extra_tags}"
 }
