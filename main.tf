d1f3726d7c7a505464f749396ecf15a61b085964
MohitSainiS <mohit.saini.snaatak@mygurukulam.co>
2025-07-16 13:13:35 +0530

    fix hdd code

--- main.tf
+++ main.tf
@@ -2,26 +2,28 @@ resource "proxmox_vm_qemu" "proxmox_vm" {
   name        = local.vm_name
   target_node = var.target_node
   vmid        = var.vm_id
+  clone       = var.ami
+  full_clone  = true
+  scsihw      = var.scsi_hw
+  onboot      = var.onboot
 
   cpu {
     cores = var.cpu_core
   }
 
   memory = var.memory_size
-  clone  = var.ami
-  scsihw = var.scsi_hw
-  onboot = true
 
-  disks {
-    scsi {
-      scsi0 {
-        disk {
-          size    = var.disk_size
-          storage = var.storage
-        }
-      }
-    }
-  }
+  agent     = var.enable_agent
+  skip_ipv6 = true
+
+disk {
+  slot     = var.disk_slot       
+  type     = var.disk_type       
+  storage  = var.storage         
+  size     = var.disk_size       
+  format   = var.disk_format     
+  iothread = var.disk_iothread   
+}
 
   network {
     id       = 0
