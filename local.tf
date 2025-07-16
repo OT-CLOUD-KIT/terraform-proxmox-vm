c6989f99ca765f0ace7fe40bc6457b253270d6c7
MohitSainiS <mohit.saini.snaatak@mygurukulam.co>
2025-07-16 13:15:23 +0530

    fix hdd code

--- local.tf
+++ local.tf
@@ -1,4 +1,4 @@
 locals {  
   vm_name = "${var.environment}-${var.location}-${var.role}-${var.identifier}-${var.vertical}"
-  tags = "${var.environment};${var.location};${var.role};${var.identifier};${var.vertical};${var.owner};${var.availability};${var.lifetime};${var.operating_system};${var.application}"
+  tags = "${var.environment};${var.location};${var.role};${var.identifier};${var.vertical};${var.owner};${var.availability};${var.lifetime};${var.operating_system};${var.extra_tags}"
 }
