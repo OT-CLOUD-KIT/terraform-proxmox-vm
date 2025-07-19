53c94992f5c1c95ea7444984232ccf84bb79ee79
MohitSainiS <mohit.saini.snaatak@mygurukulam.co>
2025-07-19 21:34:08 +0530

    fix code

--- main.tf
+++ main.tf
@@ -2,25 +2,26 @@ resource "proxmox_vm_qemu" "proxmox_vm" {
   name        = local.vm_name
   target_node = var.target_node
   vmid        = var.vm_id
+  clone       = var.ami
+  full_clone  = true
+  scsihw      = "virtio-scsi-single"
+  onboot      = true
 
   cpu {
     cores = var.cpu_core
   }
 
-  memory = var.memory_size
-  clone  = var.ami
-  scsihw = var.scsi_hw
-  onboot = true
+  memory    = var.memory_size
+  agent     = 1
+  skip_ipv6 = true
 
-  disks {
-    scsi {
-      scsi0 {
-        disk {
-          size    = var.disk_size
-          storage = var.storage
-        }
-      }
-    }
+  disk {
+    slot     = "scsi0"
+    type     = "disk"
+    storage  = "local"
+    size     = var.disk_size
+    format   = "qcow2"
+    iothread = true
   }
 
   network {
