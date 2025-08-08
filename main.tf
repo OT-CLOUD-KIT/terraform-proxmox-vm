9f2f91696e0b44beff8869dc87dd076c20239d70
MohitSainiS <mohit.saini.snaatak@mygurukulam.co>
2025-08-08 13:33:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,35 +1,37 @@
 resource "proxmox_vm_qemu" "proxmox_vm" {
   name        = local.vm_name
-  target_node = var.target_node
+  target_node = "hercules"
   vmid        = var.vm_id
   clone       = var.ami
   full_clone  = true
-  scsihw      = "virtio-scsi-single"
-  onboot      = true
+  scsihw      = var.scsi_hw
+  onboot      = var.onboot
 
   cpu {
     cores = var.cpu_core
   }
 
-  memory    = var.memory_size
-  agent     = 1
-  skip_ipv6 = true
+  memory     = var.memory_size
+  agent      = 1
+  skip_ipv6  = var.skip_ip
 
   # disk {
-  #   slot     = "scsi0"
-  #   type     = "disk"
-  #   storage  = "local"
+  #   slot     = var.disk_slot
+  #   type     = var.disk_type
+  #   storage  = var.storage
   #   size     = var.disk_size
-  #   format   = "qcow2"
-  #   iothread = true
+  #   format   = var.disk_format
+  #   iothread = var.disk_iothread
   # }
 
   # network {
-  #   id       = 0
-  #   model    = "virtio"
-  #   firewall = true
-  #   bridge   = "vmbr0"
+  #   id       = var.network_id
+  #   model    = var.network_model
+  #   firewall = var.network_firewall
+  #   bridge   = var.network_bridge
   # }
 
   tags = local.tags
+
+ 
 }
